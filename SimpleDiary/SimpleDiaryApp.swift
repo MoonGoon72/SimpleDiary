31fd7b7f075c7d9116a3713cb5d6e0b2ce5c2c4a
MoonGoon <moongoon.cnu@gmail.com>
2023-02-13 16:34:58 +0900

    Initial Commit

---
+++ SimpleDiary/SimpleDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDiaryApp.swift
+//  SimpleDiary
+//
+//  Created by cnu on 2023/02/13.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
